2dcc07b562fee98d05f6679bf18825af60dbc552
Asaduzzaman Shuvro <shuvro@blockbonds.io>
2018-01-17 15:12:04 +0600

    tableView added to show the datas

--- MultipleEnvironment/HomeVC.swift
+++ MultipleEnvironment/HomeVC.swift
@@ -8,16 +8,55 @@
 
 import UIKit
 
-class HomeVC: UIViewController {
+class HomeVC: UIViewController,UITableViewDelegate, UITableViewDataSource {
 
+    var tableView:  UITableView!
+    var datas: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView = UITableView(frame: CGRect(x: 0, y: 20, width: self.view.frame.width, height: self.view.frame.height), style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        tableView.estimatedSectionFooterHeight = 0
+        self.view.addSubview(tableView)
+        
+        if let infoDic = Bundle.main.infoDictionary {
+            for (key,valu) in infoDic {
+                
+                if let strValue = valu as? String {
+                    datas.append("\(key) --> \(strValue)")
+                }
+                
+            }
+            tableView.reloadData()
+//            print(infoDic.keys)
+//            print(infoDic.values)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+            cell?.textLabel?.text = datas[indexPath.row]
+        }
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datas.count
     }
 
 
